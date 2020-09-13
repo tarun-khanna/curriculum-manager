20676960817027d305a2197407c97f593c270ce7
tarun <tarunrajkhanna@gmail.com>
2020-09-14 02:19:47 +0530

    redundant assets deleted  + dist updated to docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './docs'),
     filename: 'bundle.[hash].js'
   },
   module: {
@@ -62,7 +62,7 @@ module.exports = {
     extensions: ['.js', '.jsx', '.json', '.wasm', '.mjs', '*'],
   },
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
+    contentBase: path.join(__dirname, './docs'),
     compress: true,
     port: 8000,
   },
